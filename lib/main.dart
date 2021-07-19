73970a2c55a45816849646c42b0534de579f73bf
Naveen Chandel <naveenchandel151@gmail.com>
2021-07-19 17:08:13 +0530

    first Commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/app/landing_page.dart';
+import 'package:todo/services/auth.dart';
+
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider<AuthBase>(
+      create: (context) => Auth(),
+      child: MaterialApp(
+      title: 'Time Management',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: LandingPage(),
+      ),
+    );
+  }
+}
